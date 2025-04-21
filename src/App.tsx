b715f54ab54d53f9dee44e79b73a944a6bd6d078
Nodir <nodirhikmatov6gmail.com>
2025-04-22 01:46:10 +0900

    feat: develop screen based components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
 import React from "react";
 import "./css/app.css";
+import { Link, Routes, Route } from "react-router-dom";
+import HomePage from "./app/screens/helpPage/index";
+import OrdersPage from "./app/screens/OrdersPage/index";
+import UserPage from "./app/screens/userPage/index";
+import ProductsPage from "./app/screens/productsPage/index";
 
 function App() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">HomePage</Link>
+          </li>
+          <li>
+            <Link to="/products">ProductsPage</Link>
+          </li>
+          <li>
+            <Link to="/orders">OrdersPage</Link>
+          </li>
+          <li>
+            <Link to="/member-page">UserPage</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/member-page" element={<UserPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter } from "react-router-dom"; // ✅ Add this
 import "./css/index.css";
+import theme from "./theme";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        {" "}
+        {/* ✅ Wrap your app here */}
+        <ThemeProvider theme={theme}>
+          <App />
+          <CssBaseline />
+        </ThemeProvider>
+      </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
